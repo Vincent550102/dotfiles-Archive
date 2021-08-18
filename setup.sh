935f4d5600572b0d8ce768c5e4f63b6e18ba7bd1
Vincent550102 <5020559@gmail.com>
2021-08-18 14:39:06 +0800

    upd gather

--- setup.sh
+++ setup.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 #start config vimrc
-echo "start config vimrc"
 cp .vimrc ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/abertsch/Menlo-for-Powerline.git
 mkdir ~/.fonts
-cd Menlo-for-Powerline/
-cp "Menlo for Powerline.ttf" ~/.fonts/
+cp Menlo-for-Powerline/"Menlo for Powerline.ttf" ~/.fonts/
 fc-cache -vf ~/.fonts
-cd ~/.vim/bundle
 git clone git://github.com/altercation/vim-colors-solarized.git
+cp vim-colors-solarized ~/.vim/bundle
 vim +PluginInstall +qall
-
+rm -rf Menlo-for-Powerline
+rm -rf vim-colors-solarized
 
